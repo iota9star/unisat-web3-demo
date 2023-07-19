252d382f0ad594a0d5f08396ee2c22aab22f597a
iota9star <iota9star@foxmail.com>
2023-07-19 17:51:47 +0800

    ✨ Add more

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import eruda from "eruda";
+
+eruda.init();
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
